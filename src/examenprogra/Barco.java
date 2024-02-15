448d73dde67581e2253c5257fa657c8bf4f6b449
balto <balto@DESKTOP-RMMOPHK>
2024-02-15 11:46:50 -0600

    examen

---
+++ src/examenprogra/Barco.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenprogra;
+
+/**
+ *
+ * @author balto
+ */
+import java.util.Date;
+
+abstract class Barco {
+    private String nombre;
+    private Date fechaDeCirculacion;
+
+    public Barco(String nombre) {
+        this.nombre = nombre;
+        this.fechaDeCirculacion = new Date();
+    }
+
+    public final String getNombre() {
+        return this.nombre;
+    }
+
+    public final Date getFechaDeCirculacion() {
+        return this.fechaDeCirculacion;
+    }
+
+    @Override
+    public String toString() {
+        return this.nombre;
+    }
+
+    public abstract void agregarElemento();
+
+    public abstract double vaciarCobrar();
+
+    public abstract double precioElemento();
+}
+
---
+++ src/examenprogra/TipoPesquero.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenprogra;
+
+/**
+ *
+ * @author balto
+ */
+public enum TipoPesquero {
+    PEZ(200),CAMARON(300),LANGOSTA(500);
+    
+     private final double precio;
+
+    TipoPesquero(double precio) {
+        this.precio = precio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+}
